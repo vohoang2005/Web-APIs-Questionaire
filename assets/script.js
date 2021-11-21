2eeb74cdd51776cea5de2ade3962139a05a697c4
vohoang2005 <vohoang2005@gmail.com>
2021-11-21 13:50:09 -0800

    update HS

--- assets/script.js
+++ assets/script.js
@@ -50,7 +50,7 @@ getScore();
 
 function getScore() {
 var quizContent = `
-<h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
+<h2>` + localStorage.getItem("highscoreName") + ` highscore:</h2>
 <h1>` + localStorage.getItem("highscore") + `</h1><br> 
 
 <button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
